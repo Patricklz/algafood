ec21001973a7f4e5564cb8fa85b44df71fd685c6
Patrick <patrick_luiz@msn.com>
2020-10-03 11:39:23 -0300

    spring

---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Restaurante {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
+	
+	@ManyToOne
+	private Cozinha cozinha;
+
+
+	
+}
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Component
+public class RestauranteRepositoryImpl implements RestauranteRepository{
+
+	@Override
+	public List<Restaurante> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurante buscar(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Restaurante salvar(Restaurante restaurante) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(Restaurante restaurante) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
